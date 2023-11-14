400b9597295268dc3bc246e79f006b37e1c8183e
Turmunkh <turmunkhn@nestsolutions.llc>
2023-11-13 16:06:11 -0800

    Cleaned the first page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,14 @@
 "use client";
-import type { Metadata } from "next";
-import { Inter, Roboto_Flex } from "next/font/google";
-// import "./globals.css";
-import { Firstpage } from "./firstSection/firstpage";
-import { Secondpage } from "./secondSection/secondPage";
-import { useState } from "react";
-import { Thirdpage } from "./thirdSection/thirdPage";
-import { FourthPage } from "./fourthPage/fourthPage";
-import { FifthPage } from "./fifthSection/fifthPage";
 import { Footer } from "./Footer/footer";
+import { FifthPage } from "./fifthSection/fifthPage";
+import { Firstpage } from "./firstSection/firstpage";
+import { FourthPage } from "./fourthPage/fourthPage";
+import { Secondpage } from "./secondSection/secondPage";
+import { Thirdpage } from "./thirdSection/thirdPage";
 
-const inter = Inter({ subsets: ["latin"] });
-
-<style>
-  @import
-  url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
-</style>;
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [show, setShow] = useState(false);
+export default function RootLayout() {
   return (
     <div>
-      {/* {show ? <Firstpage /> : null}
-      <button onClick={() => setShow(!show)}>Click!</button> */}
       <Firstpage />
       <Secondpage />
       <Thirdpage />
