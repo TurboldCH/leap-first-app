57e789232d2a02bec14b36d20a3c1636ad1dd75f
Turbold Chuluunbaatar <turboch@Turbolds-MacBook-Pro.local>
2023-11-27 21:17:59 -0600

    Created Post Input

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -3,9 +3,12 @@
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { useState } from "react";
 import { firebaseApp } from "../../firebase";
-import { TextField, styled } from "@mui/material";
 import { Footer } from "@/app/Footer/footer";
 import React from "react";
+import { getDownloadURL, ref, getStorage, uploadBytes } from "firebase/storage";
+import styled from "@emotion/styled";
+import { Box, Button, TextField } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const ContainerStyle = styled("div")({
   display: "flex",
@@ -15,22 +18,65 @@ const ContainerStyle = styled("div")({
   maxWidth: "1440px",
   position: "relative",
 });
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 export default function Home() {
   const [url, setURL] = useState<File>();
+  const [avatar, setAvatar] = useState<File>();
   const [header, setHeader] = useState("");
   const [content, setContent] = useState("");
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
   const db = getFirestore(firebaseApp);
+  const storage = getStorage(firebaseApp);
 
-  const handleSelectedFile = (files: any) => {
+  const handleSelectedFileURL = (files: any) => {
     if (files && files[0].size < 10000000) {
       console.log(files[0], "File uploaded");
-      setURL(files[0].name);
-      console.log("URL set");
+      setURL(files[0]);
     } else {
       console.log("File size too large");
+    }
+  };
+
+  const handleSelectedFileAvatar = (files: any) => {
+    if (files && files[0].size < 10000000) {
+      console.log(files[0], "File uploaded");
+      setAvatar(files[0]);
+    } else {
+      console.log("File size too large");
+    }
+  };
+
+  const uploadFileToStorage = async () => {
+    console.log(url?.name);
+    const storageRef = ref(storage, url?.name);
+    try {
+      await uploadBytes(storageRef, url!);
+      const downloadAvatarURL = await getDownloadURL(
+        ref(storage, avatar?.name)
+      );
+      const downlaodUrl = await getDownloadURL(ref(storage, url?.name));
+      await addDoc(collection(db, "blog"), {
+        url: downlaodUrl,
+        header: header,
+        content: content,
+        name: name,
+        date: date,
+        avatar: downloadAvatarURL,
+      });
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -49,6 +95,7 @@ export default function Home() {
       >
         <h1
           style={{
+            fontFamily: "'mulish', sans-serif",
             paddingTop: "200px",
             width: "463px",
             display: "flex",
@@ -92,53 +139,95 @@ export default function Home() {
           gap: "30px",
         }}
       >
-        <h1>Create a post</h1>
-        <div>
-          <input
-            type="file"
-            placeholder="Background file"
-            onChange={(event) => {
-              handleSelectedFile(event.target.files);
-              console.log(event.target.files, "Event target files");
+        <h1 style={{ fontFamily: "'mulish', sans-serif" }}>Create a post</h1>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            alignItems: "center",
+          }}
+        >
+          <div style={{display:"flex", gap: "10px"}}>
+            <Button
+              onChange={(event) => {
+                handleSelectedFileURL(event.target.files);
+              }}
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload URL
+              <VisuallyHiddenInput type="file" />
+            </Button>
+            <Button
+              onChange={(event) => {
+                handleSelectedFileAvatar(event.target.files);
+              }}
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload Avatar
+              <VisuallyHiddenInput type="file" />
+            </Button>
+          </div>
+          <Box
+            style={{ display: "flex", flexDirection: "column" }}
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "65ch" },
             }}
-          />
-          <input
-            onChange={(event) => {
-              setHeader(event.target.value);
-            }}
-            placeholder="Title"
-          />
-          <input
-            onChange={(event) => {
-              setContent(event.target.value);
-            }}
-            placeholder="Content"
-          />
-          <input
-            onChange={(event) => {
-              setName(event.target.value);
-            }}
-            placeholder="Your name"
-          />
-          <input
-            onChange={(event) => {
-              setDate(event.target.value);
-            }}
-            placeholder="Date"
-          />
-          <button
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              required
+              id="outlined-required"
+              label="Header"
+              placeholder="Your Title Goes Here"
+              onChange={(event) => {
+                setHeader(event.target.value);
+              }}
+            />
+            <TextField
+              required
+              id="outlined-disabled"
+              label="Content"
+              placeholder="Your Content Goes Here"
+              onChange={(event) => {
+                setContent(event.target.value);
+              }}
+            />
+            <TextField
+              required
+              id="outlined-password-input"
+              label="Name"
+              placeholder="Your name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+            <TextField
+              required
+              id="outlined-read-only-input"
+              label="Date"
+              placeholder="Januray 21, 2023"
+              onChange={(event) => {
+                setDate(event.target.value);
+              }}
+            />
+          </Box>
+          <Button
+            style={{ width: "200px" }}
+            variant="outlined"
             onClick={async () => {
-              await addDoc(collection(db, "blog"), {
-                url: url || null,
-                header: header,
-                content: content,
-                name: name,
-                date: date,
-              });
+              uploadFileToStorage();
+              alert("Saved Successfully!");
             }}
           >
             Save
-          </button>
+          </Button>
         </div>
       </div>
     </div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,7 +9,6 @@ import { firebaseApp } from "../firebase";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { getFirestore, collection } from "firebase/firestore";
 
-
 const DivStyle = styled("div")({
   background: "var(--HP-design-color, #F5F7FA)",
   display: "flex",
@@ -40,90 +39,6 @@ const ContentStyle = styled("div")({
 });
 
 export default function RootLayout() {
-  // const posts = [
-  //   {
-  //     url: "/blog/blog1.png",
-  //     header: "The Emotional Toll of Being in UX",
-  //     content:
-  //       "There are times when our work impacts us deeply — sometimes in ways we neither acknowledge nor understand",
-  //     avatar: "/blog/wade.png",
-  //     name: "Wade Warren",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog2.png",
-  //     header: "10 secret tips for managing a team remotely",
-  //     content:
-  //       "Our latest updates and blogs about managing your team Our latest updates and blogs about managing your team",
-  //     avatar: "/blog/bessie.png",
-  //     name: "Bessie Cooper",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog3.png",
-  //     header: "What Ever Happened to Steampunk?",
-  //     content:
-  //       "How the iPhone popularized steampunk… and how the iPhone killed it off",
-  //     avatar: "/blog/courtney.png",
-  //     name: "Courtney Henry",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog4.png",
-  //     header: "The 2020 Guide for White Men in Tech",
-  //     content:
-  //       "How to use centuries of unfair advantage to make the world a more equal place",
-  //     avatar: "/blog/darlene.png",
-  //     name: "Darlene Robert",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog5.png",
-  //     header: "Why Are Women Still Behind in the Design World?",
-  //     content:
-  //       "It’s 2020, but women designers still face lower pay and less opportunity. What gives?",
-  //     avatar: "/blog/leslie.png",
-  //     name: "Leslie Alexander",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog6.png",
-  //     header: "The Ultimate Guide to Becoming a Great Designer",
-  //     content:
-  //       "Our latest updates and blogs about managing your team Our latest updates and blogs about managing your team",
-  //     avatar: "/blog/eleanor.png",
-  //     name: "Eleanor Pena",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog7.png",
-  //     header: "Data-Driven Design is Killing Our Instincts",
-  //     content:
-  //       "Our latest updates and blogs about managing your team Our latest updates and blogs about managing your team",
-  //     avatar: "/blog/jane.png",
-  //     name: "Jane Cooper",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog8.png",
-  //     header: "Finding the Blank Spots in Big Data",
-  //     content:
-  //       "Artists and designers are working to address a major problem for marginalized communities in the data economy: ‘If the data does not exist…",
-  //     avatar: "/blog/jenny.png",
-  //     name: "Jenny Wilson",
-  //     date: "2nd January,2022",
-  //   },
-  //   {
-  //     url: "/blog/blog9.png",
-  //     header: "3 Lessons We Learned From Rebranding Gusto",
-  //     content:
-  //       "You might not know the music, but designers have certainly taken notice of this small but visceral movement from the late ’80s",
-  //     avatar: "/blog/jacob.png",
-  //     name: "Jacob Jones",
-  //     date: "2nd January,2022",
-  //   },
-  // ];
-
   const [value, loading, error] = useCollection(
     collection(getFirestore(firebaseApp), "blog"),
     {
@@ -151,13 +66,13 @@ export default function RootLayout() {
         </ContentStyle>
         <Posts_style>
           {error && (
-            <h1 style={{ backgroundColor: "black", color: "white" }}>
-              Error...
+            <h1 style={{ color: "black" }}>
+              ERROR...
             </h1>
           )}
           {loading && (
-            <h1 style={{ backgroundColor: "black", color: "white" }}>
-              Loading...
+            <h1 style={{ color: "black" }}>
+              LOADING...
             </h1>
           )}
           {!loading &&
